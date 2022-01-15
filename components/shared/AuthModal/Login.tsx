f188824a6537b3d559ba3b25886f955aa3dd8716
Jay Philip <jayphilipcamillo@gmail.com>
2022-01-15 21:41:09 +0800

    Remove comments in Login component

--- components/shared/AuthModal/Login.tsx
+++ components/shared/AuthModal/Login.tsx
@@ -1,14 +1,12 @@
 import { Center, Text, HStack } from "@chakra-ui/react";
 import FacebookLoginButton from "./FacebookLoginButton";
 import GoogleLoginButton from "./GoogleLoginButton";
-// import FacebookLoginButton from "../FacebookLoginButton";
-// import GoogleLoginButton from "../GoogleLoginButton";
 import LocalLoginForm from "./LocalLoginForm";
+
 const Login = () => {
   return (
     <>
       <LocalLoginForm />
-      {/* <Divider orientation='horizontal' my='8' /> */}
       <Text textAlign='center' color='gray.500' mt='8' mb='4'>
         Or Login With:
       </Text>
