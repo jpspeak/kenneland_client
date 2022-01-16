e464fc549eaf4e48c72b3aee22ca43ece985a542
Jay Philip <jayphilipcamillo@gmail.com>
2022-01-16 20:38:52 +0800

    Add isTruncated to text in KennelItem

--- components/pages/kennels/KennelsItem.tsx
+++ components/pages/kennels/KennelsItem.tsx
@@ -14,18 +14,18 @@ interface KennelItemProps {
 const KennelItem = ({ _id, name, displayPicture, location }: KennelItemProps) => {
   return (
     <Link href={`/kennels/${_id}`} passHref>
-      <Box cursor='pointer' width='100%' bgColor='white' shadow='xs' rounded='lg' py='4' px='4' _hover={{ md: { shadow: "lg" } }}>
+      <Box overflow='hidden' cursor='pointer' width='100%' bgColor='white' shadow='xs' rounded='lg' py='4' px='4' _hover={{ md: { shadow: "lg" } }}>
         <Box borderRadius='full' pb='100%' bgColor='blackAlpha.100' shadow='xs' className='relative'>
           <Box className='absolute m-1'>
             <Image src={displayPicture || "/kennel-display-picture-default.png"} alt='Kennel displayPicture' objectFit='cover' height={500} width={500} className='rounded-full absolute' />
           </Box>
         </Box>
-        <Text textAlign='center' noOfLines={1} fontWeight='semibold' fontSize={{ base: "xs", sm: "sm" }} mt='3'>
-          {name} sdf sdfsdf sdf
+        <Text textAlign='center' isTruncated fontWeight='semibold' fontSize={{ base: "xs", sm: "sm" }} mt='3'>
+          {name}
         </Text>
         <HStack justifyContent='center' color='blackAlpha.700' spacing='0' lineHeight='short' fontSize={{ base: "x-small", md: "xs" }}>
           <Icon as={HiOutlineLocationMarker} />
-          <Text noOfLines={1}>{location}</Text>
+          <Text isTruncated>{location}</Text>
         </HStack>
       </Box>
     </Link>
